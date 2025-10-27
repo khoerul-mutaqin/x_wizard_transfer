913f416e8855ce8b919a49cd947bf357eebcb24b
FWA-Khoerul.MUTAQIN <khoerul.mutaqin@falinwa.com>
2025-10-27 09:38:12 +0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "x_wizard_transfer",
+    "version": "18.0.0.0.0",    
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'description': """
+Long description of module's purpose
+    """,
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base', 'stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+
+}
+
